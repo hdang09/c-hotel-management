923294387f2e3ccc4fe668818d94e996225605b8
DoanVietThanh <93387761+DoanVietThanh@users.noreply.github.com>
2022-03-12 22:11:58 +0700

    Update exit.h

--- exit.h
+++ exit.h
@@ -1,8 +1,21 @@
 void exiting() {
 	system("cls");
-	printf("THANK YOU FOR TRUSTING OUR SERVICES");
-	for (int i = 0; i < 25; i++) printf("-");
-	printf("\nPress any key to exit");
+	textcolor(12);
+	printf("\n\n\n  %c", 201);
+	for (int i = 0; i <= 63; i++) {
+		printf("%c", 205);
+	}
+	printf("%c\n", 187);
+	printf("  %c \t\t                                   \t\t   %c\n", 186, 186);
+	printf("  %c \t\tTHANK YOU FOR TRUSTING OUR SERVICES\t\t   %c\n", 186, 186);
+	printf("  %c \t\t                                   \t\t   %c\n", 186, 186);
+	printf("  %c \t\t    %c  FCODE - CODE THE DREAM  %c     \t\t   %c\n", 186, 3, 3, 186);
+	printf("  %c \t\t                                   \t\t   %c\n", 186, 186);
+	printf("  %c", 200);
+	for (int i = 0; i <= 63; i++) {
+		printf("%c", 205);
+	}
+	printf("%c", 188);
 	getch();
 	exit(0);
 }
